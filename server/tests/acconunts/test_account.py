7c56ff8e559bda22f8096309c8a7f1f9f6c15b90
Kiuliumov <miti.bania@gmail.com>
2026-08-01 17:47:04 +0300

    Added tests

---
+++ server/tests/acconunts/test_account.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_get_account(authenticated_client):
+    response = authenticated_client.get(
+        "/accounts/me/"
+    )
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_update_account(authenticated_client):
+    response = authenticated_client.patch(
+        "/accounts/me/",
+        {
+            "bio": "Updated bio",
+            "phone": "+987654321",
+        },
+        format="json",
+    )
+
+    assert response.status_code == 200
+
+    assert response.data["bio"] == "Updated bio"
+    assert response.data["phone"] == "+987654321"
+
+
+@pytest.mark.django_db
+def test_account_requires_authentication(api_client):
+    response = api_client.get(
+        "/accounts/me/"
+    )
+
+    assert response.status_code == 401
